136961574999838c7065d0c88e19145ed20ee598
Arthur020104 <arthur020104@gmail.com>
2024-09-28 22:26:46 -0300

    Adicionando funcoes de login e registro, alem de configuracao de rota

--- Learning/Apreender/urls.py
+++ Learning/Apreender/urls.py
@@ -3,4 +3,8 @@ from django.urls import path
 from django.contrib import admin
 from . import views
 
-urlpatterns = [ path('', views.index, name='index'),]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.loginView, name='login'),
+    path('register/', views.register, name='register'),
+               ]
--- Learning/Apreender/views.py
+++ Learning/Apreender/views.py
@@ -1,19 +1,76 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Topic
+from .models import Topic, Subject, User
+from django.contrib.auth import authenticate, login
+from django.urls import reverse
 
 
 def index(request):
-    #return index page
-    request.session['user'] = 'user'
-    problemsSuggestForToday = []
-    # Get all topics for this user
-    print(request.user)
+    # Verificar se o usuário está logado
     if str(request.user) == 'AnonymousUser':
         return render(request, 'Apreender/index.html', {'problems': None})
-    topics = Topic.objects.filter(subject__owner=request.user)
-    for topic in topics:
-        askForMoreProblems, problems = topic.suggestNext()
-        problemsSuggestForToday.append(problems)
+    
+    # Obter o usuário
+    user = User.objects.get(username=request.user.username)
+    
+    # Obter todos os assuntos do usuário
+    subjects = Subject.objects.filter(owner=user)
+    
+    problemsSuggestForToday = []
+
+    # Iterar sobre todos os assuntos e sugerir problemas
+    for subject in subjects:
+        topics = Topic.objects.filter(subject=subject)
+        for topic in topics:
+            askForMoreProblems, problems = topic.suggestNext()
+            problemsSuggestForToday.append(problems)
+
+    # Renderizar a página com os problemas sugeridos
     return render(request, 'Apreender/index.html', {'problems': problemsSuggestForToday})
-    #return render(request, 'Apreender/index.html')
+
+
+
+def loginView(request):
+    if request.method == 'GET':
+        return render(request, 'Apreender/login.html')
+
+    elif request.method == 'POST': 
+        username = request.POST['name'] 
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is None:
+            return render(request, "Apreender/login.html", {
+                "errorMessage": "Nome de usuário e/ou senha inválidos."
+            })
+        else:
+            login(request, user)
+            return redirect(reverse("index"))
+
+
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'Apreender/register.html')
+
+    elif request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['confirm-password']
+
+        if password != password2:
+            return render(request, "Apreender/register.html", {
+                "errorMessage": "As senhas não conferem."
+            })
+
+        if User.objects.filter(username=username).exists():
+            return render(request, "Apreender/register.html", {
+                "errorMessage": "Nome de usuário já existe."
+            })
+
+        user = User.objects.create_user(username=username, email=email, password=password)
+        user.save()
+        
+        login(request, user)
+        return redirect(reverse("index"))
